93cd006b8f5193fa72397fcaa7f9ac25bdcce5d3
Fernando Flach <fernandoflach88@gmail.com>
2024-07-30 18:29:52 -0300

    Add permanent color change when hovering the squares.

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ for (let i = 0; i< 16; i++){
     let squareContainer = document.createElement("div")
 
     for (let i = 0; i < 16; i++) {
+
         let square = document.createElement("div");
         square.classList.add("square");
-        square.innerText = "Test"
+        //square.innerText = "Test"
+        square.addEventListener("mouseover", () => {square.style.backgroundColor = "black"})
     
         squareContainer.appendChild(square);
     }
     
     container.appendChild(squareContainer);
+
+    let hovered = document.querySelector(".square:hover");
+    if (hovered != null) {
+        hovered.textContent = "hovered";
+        hovered.style.backgroundColor = "black";
+    }
+ 
 }
+
