5900207386a6dd7089e77457c0508a4e42ccbae8
Fernando Flach <fernandoflach88@gmail.com>
2024-07-30 18:13:32 -0300

    Create divs for each row, to ensure grid is square.
    Also added hover effect on each square.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.innerText = "Test"
 
-    container.appendChild(square);
+for (let i = 0; i< 16; i++){
+
+    let squareContainer = document.createElement("div")
+
+    for (let i = 0; i < 16; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.innerText = "Test"
+    
+        squareContainer.appendChild(square);
+    }
+    
+    container.appendChild(squareContainer);
 }
-
