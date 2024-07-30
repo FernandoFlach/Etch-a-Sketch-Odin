ddf4fae68d5a9676e90816a45e434438615ec9c3
Fernando Flach <fernandoflach88@gmail.com>
2024-07-30 16:56:27 -0300

    Add stylesheet to style the 16 squares of the screen.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const container = document.querySelector("#container");
+for (let i = 0; i < 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.innerText = "Test"
+
+    container.appendChild(square);
+}
+
